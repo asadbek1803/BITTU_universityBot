2eaee6e3303e644f6cd7a22eba33849b003168a1
Asadbek <asadbekoffical1202@gmail.com>
2025-03-01 16:30:00 +0500

    Update: add channels

--- middlewares/SubscriptionMiddleware.py
+++ middlewares/SubscriptionMiddleware.py
@@ -179,7 +179,7 @@ class SubscriptionMiddleware(BaseMiddleware):
             # Maxfiy havola tekshirish
             if channel.startswith("https://t.me/+"):
                 # Maxfiy havolalar uchun so'rov yuborish imkoni yo'q
-                return False
+                return True
                 
             # Kanalning username qismini olish
             channel_username = channel
